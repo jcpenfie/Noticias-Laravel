644b8ea0ae35b1978bc05beeff99e20338b71a1a
jcpenfie <juancarlospfierro@gmail.com>
2022-02-16 10:31:33 +0100

    rutas organizadas

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,55 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+
+//Todas las páginas tiene sus respectivas paginaciones
+
+//--------------------Pagina principal (sin usuario)-----------------------------
+// Pagina con todas la noticas sin importar la categoría
 Route::get('/', [HomeController::class,'index'])->name('paginaPrincipal');
-Route::get('noticias/{idcategoria}', [HomeController::class,'index'])->name('noticias');
-Route::get('login/create', [HomeController::class,'create'])->name('login.create'); 
-Route::post('login/store/{$noticia}', [HomeController::class,'store'])->name('login.store');
-Route::get('login/{idusuario?}', [HomeController::class,'login'])->name('login');
+
+//Muestra todas la noticias de cierta categoria
+Route::get('noticias/{idcategoria}', [HomeController::class,'categoria'])->name('noticias.categoria');
+
+
+//--------------------Login al panel de control-----------------------------
+
+//Login que pide usuario y contraseña
+Route::get('login', [HomeController::class,'login'])->name('login');
+
+
+//--------------------Panel de control (usuarios)-----------------------------
+
+
+//Panel de control: Lista de todas las noticias de ese usuario y puede filtrar por categoria
+//(si es admin aparecen todas las noticias y un nuevo filtro por autor)
+Route::get('login/{idusuario?}', [HomeController::class,'usuario'])->name('login.idusuario');
+
+
+//>>>>>>>>>>>>>>>>>>--------------------Operaciones de las noticias-----------------------------
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--------------------Botones: borrar y editar y ver-----------------------------
+
+//En el panel de control hay un botón borrar que llama a este método con la id que hay que borrar
 Route::delete('login/destroy/{idnoticia}', [HomeController::class,'destroy'])->name('login.destroy');
+
+//Te manda a un formulario con los datos ya introducidos en él para modificar exactamente lo que quieres
 Route::put('login/update/{idnoticia}', [HomeController::class,'update'])->name('login.update');
 
-// Route::get('login/create/{idnoticia}', [HomeController::class,'create'])->name('create');
+//Lista cierta noticia de cierto autor(usuario)
+Route::put('login/{idusuario}/{idnoticia}', [HomeController::class,'showNot'])->name('login.show.noticia');
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--------------------Boton: Nueva Noticia-----------------------------
+
+//Formulario para crear una noticia
+Route::get('login/create', [HomeController::class,'create'])->name('login.create');
+
+//Recoge los datos del formulario del create y crea la noticia
+Route::post('login/store/{$noticia}', [HomeController::class,'store'])->name('login.store');
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--------------------Filtros: Categoria y Autor-----------------------------
+
+//Lista las noticias de cierto usuario de cierta categoría
+Route::post('login/{$idusuario}/{$idcategoria}', [HomeController::class,'showCat'])->name('login.show.categoria');
