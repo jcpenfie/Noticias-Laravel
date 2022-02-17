7ecc2394896db7776ea35b5efae900000f577ce7
jcpenfie <juancarlospfierro@gmail.com>
2022-02-17 13:10:55 +0100

    Merge branch 'main' of https://github.com/jcpenfie/Noticias-Laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,12 +19,6 @@
                 <div class="px-4 py-2">
                   <h1 class="text-xl font-gray-700 font-bold">{{$noticia->titulo}}</h1>
                   <div class="flex space-x-2 mt-2">
-                    <span>
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                      </svg>
-                    </span>
                     <h3 class="text-lg text-gray-600 font-semibold mb-2">{{$noticia->categoria_id}}</h3>
                   </div>
                   <p class="text-sm tracking-normal">{{$noticia->descripcion}}</p>
