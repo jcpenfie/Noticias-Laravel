eb916ef151b8577494017f35ff3bb83285ea022d
jcpenfie <juancarlospfierro@gmail.com>
2022-03-01 20:41:43 +0100

    noticia

--- app/Models/noticia.php
+++ app/Models/noticia.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Noticia extends Model
+class noticia extends Model
 {
     use HasFactory;
 }
