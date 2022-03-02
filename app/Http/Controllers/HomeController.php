2800dc6433dbb0ca7dc3b4aeec03d86b9f14756c
jcpenfie <juancarlospfierro@gmail.com>
2022-03-02 18:10:09 +0100

    mejora orden noticias index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,8 +13,7 @@ class HomeController extends Controller
     {
         // $noticias = Noticia::paginate(9);
         $categorias = Categoria::all(); //Necesario para la barra de navegacion
-        $noticias = Noticia::join('categorias', 'categorias.id', '=', 'noticias.categoria_id')
-            ->select(['noticias.*', 'categorias.nombre'])->paginate(9); //Join con las noticias y las cagegorias
+        $noticias = Noticia::orderBy('updated_at', 'desc')->Paginate(9);
         return view('index', compact('noticias','categorias'));
     }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
                                         </h1>
                                         <div class="flex space-x-2 mt-2">
                                             <h3 class="text-lg text-gray-600 font-semibold mb-2">
-                                                {{ $noticia->nombre }}</h3>
+                                                {{ $categorias[$noticia->categoria_id - 1]->nombre }}</h3>
                                         </div>
                                         <p class="text-sm tracking-normal">
                                             {{ substr($noticia->descripcion, 0, 200) . '...' }}</p>
